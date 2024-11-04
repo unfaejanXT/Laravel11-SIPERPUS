b2bfb70f0f4a15917fb42373f9e415a98e886733
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2024-11-05 05:33:25 +0700

    Implementasi Role Pustakawan

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <!-- Role Links Pustakawan -->
+                @hasrole('pustakawan')
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link :href="route('role')" :active="request()->routeIs('role')">
+                            {{ __('Role') }}
+                        </x-nav-link>
+                    </div>
+                @endhasrole
             </div>
 
             <!-- Settings Dropdown -->
